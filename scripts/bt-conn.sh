1598c71efdd6561ff77618675df88a627e80680d
Mateusz Reszka <mateusz.reszka@outlook.com>
2022-07-24 15:13:45 +0200

    scripts moved to separated repository

--- scripts/bt-conn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-devices=$(bluetoothctl devices | awk '{printf('%1 \n', $3)}')
-
-echo $devices;
--- scripts/reload-modem.sh
+++
@@ -1,4 +0,0 @@
-sudo echo 0 > /sys/bus/usb/devices/1-5/bConfigurationValue
-sleep 1
-sudo echo 3 > /sys/bus/usb/devices/1-5/bConfigurationValue 
-mmcli -L
