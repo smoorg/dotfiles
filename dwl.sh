a6b37e2e6e245add510e943abd8a5035847493ec
Mateusz Reszka <mateusz@mateuszreszka.xyz>
2025-02-10 13:06:35 +0100

    attempts to fix dwl wayland env

--- dwl.sh
+++ dwl.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+# things to do when initializing
+# dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+# systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
+# systemctl --user start pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
+
 # supposed to run after wayland session is setted, hence sleep
 post_dwl() {
-	sleep 5s
+	sleep 1s
 	waybar &
 	mako &
+	cdrepo &
 	[ -f "$WALLPAPER_FILE" ] && swaybg -i $WALLPAPER_FILE &
 }
 
 post_dwl &
-dwl > ~/.cache/dwltags &
+dbus-run-session dwl > ~/.cache/dwltags &
